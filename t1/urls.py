2c6cf6e64ce1d0f8d8cf52710d1559b00de2f208
joaquincastillo <jacastillo@uc.cl>
2019-03-22 13:12:55 -0300

    trying to bind it

--- t1/urls.py
+++ t1/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
+    re_path(r'^$', views.index, name='index'),
     path('', views.index, name='index'),
 ]

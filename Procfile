b65c26429260ff2a6c954e043271e8dae2bae3ef
joaquincastillo <jacastillo@uc.cl>
2019-03-22 12:46:47 -0300

    All cleared

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn t1integracion.wsgi
--- t1/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-import django_heroku
-
-
-class T1Config(AppConfig):
-    name = 't1'
-
-
-django_heroku.settings(locals())

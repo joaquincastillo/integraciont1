362abb88193001808bc3e03be5b11f3b13356fcf
joaquincastillo <jacastillo@uc.cl>
2019-03-22 13:27:30 -0300

    trying to bind it

--- t1/urls.py
+++ t1/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path
 
+
 from . import views
 
 urlpatterns = [
-    re_path(r'^', views.index, name='index'),
+    re_path('protected-journey-63770.herokuapp.com',views.index),
     path('', views.index, name='index'),
 ]

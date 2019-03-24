869a12fd4f1d045f5211076303482868b3285101
joaquincastillo <jacastillo@uc.cl>
2019-03-24 18:55:09 -0300

    buttons

--- t1/urls.py
+++ t1/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('t1/', views.index, name='index'),
-    path('film/<url_param>', views.show_film_page, name='film_page')
+    path('film/<url_param>/', views.show_film_page, name='film_page')
 ]
     #path(r'film/^(?P<url_param>\S+/$', views.show_film_page, name='film_page')
 ]

70cb1243872d273ba2cc913b8540f6eb2af375ec
joaquincastillo <jacastillo@uc.cl>
2019-03-24 16:11:17 -0300

    buttons

--- t1/urls.py
+++ t1/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('t1/', views.index, name='index'),
-    path('t1/', views.show_film_page, name='film_page')
+    path('', views.index, name='index'),
+    path('', views.show_film_page, name='film_page')
 ]

adeecb04a7831cd108b75f7517a515cc60954250
joaquincastillo <jacastillo@uc.cl>
2019-03-24 17:59:55 -0300

    buttons

--- t1/urls.py
+++ t1/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('t1/', views.index, name='index'),
+    path('t1/principal_page.html', views.index, name='index'),
     path(r'film/^(?P<url_param>[\w-S]+/$', views.show_film_page, name='film_page')
 ]
